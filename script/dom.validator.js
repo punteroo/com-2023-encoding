1ce99bb2b7624f817ffc9ab19232eabc6a8544c3
Lucas <mazalucas@hotmail.com>
2023-06-09 11:50:37 -0300

    feat(*): first prototype version

---
+++ script/dom.validator.js
@@ -0,0 +1,65 @@
+// Fetch the bit sequence input to disallow them from placing anything other than bits.
+function validateBitSequenceInput(input) {
+  const regex = new RegExp(/^[01]+$/);
+
+  return regex.test(input);
+}
+
+/** Listener to inputs on the bit sequence to calculate their decimal and hex counterparts. */
+document
+  .getElementById("bit-sequence-input")
+  .addEventListener("input", function (e) {
+    // Obtain input event information.
+    const { data, target } = e;
+
+    // Validate.
+    if (!validateBitSequenceInput(data))
+      // Do not allow the input to be placed.
+      target.value = target.value.slice(0, -1);
+
+    // Set the element bit-sequence-decimal to the decimal value of the input.
+    document.getElementById(
+      "bit-sequence-decimal"
+    ).innerHTML = `DEC: ${parseInt(target.value, 2).toString(10)}`;
+
+    // Set the element bit-sequence-hex to the hexadecimal value of the input.
+    document.getElementById("bit-sequence-hex").innerHTML = `HEX: ${parseInt(
+      target.value,
+      2
+    )
+      .toString(16)
+      .toUpperCase()}`;
+
+    return;
+  });
+
+/** Self-reviews for each encoding type on how I understand their inner workings. */
+document
+  .getElementById("bit-sequence-encoding")
+  .addEventListener("change", function (e) {
+    // Grab the DOM element with the description.
+    const element = document.getElementById("encoding-desc");
+
+    switch (e.target.value) {
+      case "NRZL":
+        return (element.innerHTML = `El <b>Non-Return To Zero</b> que yo hice acá es bipolar (es decir, o es voltaje alto o voltaje bajo), pero se puede implementar de forma unipolar también. El NRZL es el más simple de implementar, pero es también el que mayor problemas de continuidad tiene, debido a que no tiene mucha alternancia (manda todo como si fuera el binario lógico literal en la señal), lo que puede producir problemas de desync entre el emisor y el receptor.`);
+      case "NRZI":
+        return (element.innerHTML = `Este es una variación del <b>NRZL</b>, pero invertido. Para este caso lo que ocurre con la codificación es que, con tal de incluir un poco más de alternancia, cada valor de un bit 1 se invierte, mientras que los valores de un bit 0 se mantienen. Esto permite que la señal tenga un poco más de alternancia, pero igual presenta el mismo problema que el original (a menos que mande una cadena de puros 1s, los 0s siempre me introducen continuidad).<br>Hay 2 subgrupos de este, está el <b>NRZ-M</b> y el <b>NRZ-S</b>. Sus unicas diferencias son definir cual de los dos es el bit que alterna.`);
+      case "RZ":
+        return (element.innerHTML = `El primo hermano de los <b>NRZ</b>. Este se llama Return To Zero, y tiene una solución un poco más robusta para la solución de la continuidad. Hace que <i>cada bit tenga una alternancia de voltaje a la mitad de pulso</i>, es decir, el pulso dura la mitad de la duración del bit. Esto trae alternancia para todos los bits que se transmiten. El nombre justamente hace referencia a eso, hace mitad de pulso y luego vuelve a 0.`);
+      case "MANCH":
+        return (element.innerHTML = `Uno muy polenta, utilizado en las redes de hoy con el cable <b>Ethernet</b> (gracias Pato por el dato). Este método de codificación es el que <b>menos continuidad tiene</b>, y con mucha razón.<br/>Similar a como funciona el <b>RZ</b>, acá también el pulso durará la mitad del bit, pero introduciendo el uso de patrones y alternancia. En este caso, Manchester le asigna a todos los <b>1s</b> y <b>0s</b> un patrón de pulsos representativo, entonces podemos por ejemplo tener que el 1 es representado por medio pulso hacia arriba y medio hacia abajo, y el 0 es invertido a ese patrón.<img src="public/manchester-encoding-example.jpg"/>`);
+      case "MANCHD":
+        return (element.innerHTML = `Este también está muy bueno. Es muy, pero muy parecido a Manchester, con la diferencia que ahora no tienen patrones específicos cada bit. Existe un patrón global que se le asigna al comienzo de la comunicación a un bit, y luego existe un bit que alterna ese patrón (generalmente es el 1).<br/>Entonces, se comienza con un patrón, pero cada vez que se lee un bit <b>1</b>, se invierte y se alterna el patrón. Esto permite que la señal tenga mucha alternancia, y que no tenga problemas de continuidad.<img src="public/differential-manchester-encoding-example.jpg"/>`);
+      case "AMI":
+        return (element.innerHTML = `La codificación <b>AMI</b> plantea también una solución al problema de continuidad, pero de una forma distinta. En este caso, se utiliza un patrón de alternancia de voltaje para los bits 1, y se utiliza el 0 para representar un voltaje neutro.<br/>Entonces, si tenemos un bit 1, se alterna el voltaje (si antes el 1 se representaba con un voltaje positivo, ahora el siguiente 1 será un voltaje negativo), y si tenemos un bit 0, se mantiene el voltaje neutro.<br/>Este es un tipo de codificación <b>multipolar</b>, ya que permite tener <b>3 valores de voltaje</b> para representar cada bit.<img src="public/ami-encoding.png"/>`);
+      case "PSEUDO":
+        return (element.innerHTML = `Exactamente igual a <b>AMI</b>, solo que el bit que causa la alternancia entre voltajes altos y bajos es el 0 y no el 1.`);
+      case "B8ZS":
+        return (element.innerHTML = `Un sub-tipo validador de AMI. Introduce alternancia buscando la repetición de los bits nulos (0), y cuando encuentra una secuencia de 8 bits nulos, los reemplaza por una secuencia de 8 bits que alterna el voltaje.<br/>Esto permite que la señal tenga alternancia, pero también permite que se pueda detectar cuando se pierde la sincronización entre el emisor y el receptor, ya que si se pierde la sincronización, se pierde la secuencia de 8 bits nulos, y por ende se pierde la secuencia de 8 bits que alterna el voltaje.<br/>Al encontrar una secuencia consecutiva de 8 bits en 0, los reemplaza por el patrón <b>000VB0VB</b>, donde <b>V</b> es una violación de código (alternancia que no corresponde con las reglas de AMI) y <b>B</b> es un bit correcto con respecto al anterior.`);
+      case "HDB3":
+        return (element.innerHTML = `Un sub-tipo validador de AMI. Introduce alternancia buscando la repetición de los bits nulos (0), y cuando encuentra una secuencia de 4 bits nulos, los reemplaza por una secuencia de 4 bits en un patrón definido.<br/>Al encontrar 4 bits consecutivos en 0, aplica el patrón <b>000V</b>, donde <b>V</b> es una violación del código de AMI.<br/>Este tiene una particularidad, que es la distinción entre <b>par</b> e <b>impar</b>. Este conteo de paridad se da dependiendo de <i>cuantos pulsos pasaron desde la última vez que ocurrió una ruptura de código</i>. Dependiendo de este conteo:<ul><li>Si es par, el patrón es <b>B00V</b></li><li>Si es impar, el patrón es <b>000V</b></li></ul>`);
+      case "MLT3":
+        return (element.innerHTML = `Este tipo de codificación tiene problemas de continuidad, y se debe a que, si bien es algo extraño en comparación al resto de los otros tipos de codificación, este <b>oscila</b> entre 2 polaridades y no transmitir nada para representar los bits. Cada vez que existe un <b>1</b>, se oscilará a un valor de voltaje (V<sub>+</sub>, V<sub>0</sub> o V<sub>-</sub>) lo que lo transforma en una codificación <b>multi-polar</b>. Los bits en 0 mantendrán el último voltaje utilizado.`);
+    }
+  });
---
+++ script/encoder.js
@@ -0,0 +1,463 @@
+/** Encoding type names to match against. */
+const ENCODING_NAMES = {
+  NRZL: {},
+  NRZI: {},
+  RZ: {},
+  MANCH: {},
+  MANCHD: {},
+  AMI: {},
+  PSEUDO: {},
+  B8ZS: {},
+  HDB3: {},
+  MLT3: {},
+};
+
+/** god, js sucks so much, get me on typescript pls */
+/** Posdata: Si, escribo todo mi código en inglés por costumbre, lo siento si parece raro pero es conveniencia, chau "ñ" */
+class Encoder {
+  encoding = "";
+  bitDurationSeconds = 1.0;
+
+  constructor(encoding, bitDurationSeconds = 1.0) {
+    // Is it a valid encoding?
+    if (!Object.keys(ENCODING_NAMES).includes(encoding))
+      throw Error(`INVALID ENCODING TYPE ${encoding}!`);
+
+    this.encoding = encoding;
+    this.bitDurationSeconds = bitDurationSeconds;
+  }
+
+  /**
+   * Uses the set encoding to encode a bit sequence.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points to graph.
+   */
+  encode(bitSequence) {
+    switch (this.encoding) {
+      case "NRZL":
+        return this.generateNRZLPlotPoints(bitSequence);
+      case "NRZI":
+        return this.generateNRZIPlotPoints(bitSequence);
+      case "RZ":
+        return this.generateRZPlotPoints(bitSequence);
+      case "MANCH":
+        return this.generateMANCHPlotPoints(bitSequence);
+      case "MANCHD":
+        return this.generateMANCHDPlotPoints(bitSequence);
+      case "AMI":
+        return this.generateAMIPlotPoints(bitSequence);
+      case "PSEUDO":
+        return this.generatePSEUDOPlotPoints(bitSequence);
+      case "B8ZS":
+        return this.generateB8ZSPlotPoints(bitSequence);
+      case "HDB3":
+        return this.generateHDB3PlotPoints(bitSequence);
+      case "MLT3":
+        return this.generateMLT3PlotPoints(bitSequence);
+    }
+
+    return null;
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in NRZL) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateNRZLPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // NRZL encodes a 1 as a high voltage and a 0 as a low voltage.
+      // The voltage is held for the duration of the bit.
+      x.push(i, i + this.bitDurationSeconds);
+      y.push(bit ? bit : -1, bit ? bit : -1);
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in NRZI) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateNRZIPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // Create a flag that keeps track of the last bit voltage.
+    let flag = 1;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // Alternate the flag value based on memory.
+      flag = bit ? (flag ? 0 : 1) : flag;
+
+      // NRZI encodes a 1 as an alternating sequence, whereas 0 is the constant voltage.
+      // The voltage is held for the duration of the bit.
+      x.push(i, i + this.bitDurationSeconds);
+
+      y.push(flag, flag);
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in RZ) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateRZPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // RZ encodes all 1s as a low voltage and a high voltage for half the duration of the bit. 0s are the opposite.
+      // The voltage is held for half the duration of the bit.
+      x.push(
+        i,
+        i + this.bitDurationSeconds / 2,
+        i + this.bitDurationSeconds / 2,
+        i + this.bitDurationSeconds
+      );
+      y.push(...(bit ? [-1, -1, 0, 0] : [1, 1, 0, 0]));
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in Manchester) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateMANCHPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // Manchester takes a pattern for a bit, and inverts the pattern for the other bit.
+    // The voltage is held for the duration of the bit. Let's define the pattern for a 1.
+    const pattern1 = [1, 1, 0, 0],
+      pattern0 = [0, 0, 1, 1];
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+      // Push the plot points.
+      x.push(
+        i,
+        i + this.bitDurationSeconds / 2,
+        i + this.bitDurationSeconds / 2,
+        i + this.bitDurationSeconds
+      );
+      y.push(...(bit ? pattern1 : pattern0));
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in Differential Manchester) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateMANCHDPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // Differential Manchester is similar to Manchester, however it implements inversion.
+    // If we have a 1, we invert the pattern. If we have a 0, we keep the pattern.
+    // We implement the usage then of a flag pattern.
+    const pattern = [0, 0, 1, 1];
+
+    // Create a flag that keeps track of the last bit voltage.
+    let flag = 1;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // If the bit is 1, invert the flag.
+      flag = bit ? (flag ? 0 : 1) : flag;
+
+      // Push the plot points.
+      x.push(
+        i,
+        i + this.bitDurationSeconds / 2,
+        i + this.bitDurationSeconds / 2,
+        i + this.bitDurationSeconds
+      );
+      y.push(...(flag ? pattern : pattern.slice().reverse()));
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in AMI) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateAMIPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // AMI encodes a 1 as a high voltage, but inverts the voltage value for every 1 read. 0s just maintain no voltage.
+    let flag = 1;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // If the bit is 1, invert the flag.
+      flag = bit ? (flag != -1 ? -1 : 1) : flag;
+
+      // Push the plot points.
+      x.push(i, i + this.bitDurationSeconds);
+      y.push(bit ? flag : 0, bit ? flag : 0);
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in Pseudoternary) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generatePSEUDOPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // Pseudoternary encodes a 0 as a high voltage, but inverts the voltage value for every 0 read. 1s just maintain no voltage.
+    let flag = 1;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // If the bit is 0, invert the flag.
+      flag = bit ? flag : flag != -1 ? -1 : 1;
+
+      // Push the plot points.
+      x.push(i, i + this.bitDurationSeconds);
+      y.push(bit ? 0 : flag, bit ? 0 : flag);
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in B8ZS) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateB8ZSPlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // B8ZS is a sub-type for AMI. Since AMI does nothing about 0s, this method provides some extra alternance guarantees.
+    // Whenever there are 8 0s in a row, we replace them with a special pattern known as 000VB0VB, where V is a code violation, and B a correct bit alternance.
+    // The voltage is held for the duration of the bit.
+    let flag = 1;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Do a bitwise lookup for the 8 0s (only if we have at least 8 bits)
+      const bit8 =
+        bitSequence.slice(i, i + 8).length == 8
+          ? parseInt(bitSequence.slice(i, i + 8)) == 0
+          : false;
+
+      // Do we have to introduce a code violation?
+      if (bit8) {
+        // Introduce the violation pattern corresponding to the active flag and skip the next 7 bits.
+        for (let j = 0; j < 8; j++)
+          x.push(i + j, i + j + this.bitDurationSeconds);
+
+        // When j % 3 = 0 that means a violation must be introduced, so we negate our current flag value.
+        // If that is not the case, we just use the flag value for indexes 4 and 7.
+        // Otherwise, we just use 0 (no voltage)
+        // const bit = j % 3 == 0 && j > 0 ? -flag : [4, 7].includes(j) ? flag : 0;
+
+        // Ignore what's above (sorry pato :(, couldn't provide a high IQ dynamic solution, we're going static)
+        // We're just going to generate manually the plot points for the violation pattern.
+        y.push(
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          flag,
+          flag,
+          -flag,
+          -flag,
+          0,
+          0,
+          -flag,
+          -flag,
+          flag,
+          flag
+        );
+
+        // Skip the next 8 bits.
+        i += 8;
+      }
+
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // If the bit is 1, invert the flag.
+      flag = bit ? (flag != -1 ? -1 : 1) : flag;
+
+      // Push the plot points.
+      x.push(i, i + this.bitDurationSeconds);
+      y.push(bit ? flag : 0, bit ? flag : 0);
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in HDB3) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateHDB3PlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // HDB3 is quite different, since it distinguishes between 2 subtypes: even or odd.
+    // This parity is dictated by the amount of bits passed since the last violation ocurred.
+    // If even, the pattern is B00V
+    // If odd, the pattern is 000V
+    //  - Note (thanks Pato, again): 0 is counted as even (ALTHOUGH PARITY OF 0 IS NOT LIKE THIS IN MATH)
+    //
+    // Otherwise it works the same exact way as B8ZS.
+    let flag = 1,
+      sinceLastViolation = 0;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Do a bitwise lookup for the 4 0s (only if we have at least 4 bits)
+      const bit4 =
+        bitSequence.slice(i, i + 4).length == 4
+          ? parseInt(bitSequence.slice(i, i + 4)) == 0
+          : false;
+
+      // Do we have to introduce a code violation?
+      if (bit4) {
+        // Introduce the violation pattern corresponding to the active flag and skip the next 3 bits.
+        for (let j = 0; j < 4; j++)
+          x.push(i + j, i + j + this.bitDurationSeconds);
+
+        // Insert the pattern manually.
+        const evenPattern = [-flag, -flag, 0, 0, 0, 0, -flag, -flag],
+          oddPattern = [0, 0, 0, 0, 0, 0, flag, flag],
+          isEven = sinceLastViolation % 2 == 0 || sinceLastViolation == 0;
+
+        y.push(...(isEven ? evenPattern : oddPattern));
+
+        // Invert the bit if it's even.
+        flag = isEven ? -flag : flag;
+
+        // Reset the violation counter.
+        sinceLastViolation = 0;
+
+        // Skip the next 4 bits.
+        i += 3;
+      } else {
+        // Obtain the bit.
+        const bit = +bitSequence[i];
+
+        // If the bit is 1, invert the flag.
+        flag = bit ? (flag != -1 ? -1 : 1) : flag;
+
+        // Push the plot points.
+        x.push(i, i + this.bitDurationSeconds);
+        y.push(bit ? flag : 0, bit ? flag : 0);
+
+        sinceLastViolation++;
+      }
+    }
+
+    return { x, y };
+  }
+
+  /**
+   * Method that generates an encoded bit sequence (in MLT-3) in the form of plot points.
+   *
+   * @param {string} bitSequence The bit sequence to encode.
+   *
+   * @returns {{ x: number[], y: number[]}} An array of plot points.
+   */
+  generateMLT3PlotPoints(bitSequence) {
+    const x = [],
+      y = [];
+
+    // MLT-3 is kind of similar to NRZ-I. However, instead of using 2 levels for voltage, it uses 3.
+    // Every time a 1 is found, the voltage is reduced by 1 level. Once the level reaches low voltage, it is inverted to go back up again. Basically an oscillation.
+    // Every 0 however maintains the current voltage level.
+    let flag = 1,
+      direction = 0;
+
+    // Iterate over the bit sequence.
+    for (let i = 0; i < bitSequence.length; i++) {
+      // Obtain the bit.
+      const bit = +bitSequence[i];
+
+      // If the bit is 1, either reduce the flag by 1 if the value is 1 or add 1 to it if already at the bottom.
+      direction = bit
+        ? flag == -1 || flag == 1
+          ? !direction
+          : direction
+        : direction;
+      flag = bit ? (direction ? flag - 1 : flag + 1) : flag;
+
+      // Push the plot points.
+      x.push(i, i + this.bitDurationSeconds);
+      y.push(flag, flag);
+    }
+
+    return { x, y };
+  }
+}
